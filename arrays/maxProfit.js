3d50337b64e557f772ff8aca837edfc5653c606c
Emmanuel Sarpong <83601745+emmanuelsarpong@users.noreply.github.com>
2026-01-17 10:47:07 -0500

    Solved maxProfit problem

--- arrays/maxProfit.js
+++ arrays/maxProfit.js
@@ -4,6 +4,22 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
 
-var maxProfit = function(prices) {
-    
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else {
+      let profit = prices[i] - minPrice;
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+  return maxProfit;
 };
+
+let result = maxProfit([1, 2, 3, 4, 5, 6]);
+console.log(result);
