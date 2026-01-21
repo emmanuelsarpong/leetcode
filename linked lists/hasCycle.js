2f99549a24d9658f585498c5a8e4bd9260e9ff0c
Emmanuel Sarpong <83601745+emmanuelsarpong@users.noreply.github.com>
2026-01-21 14:46:54 -0500

    Solved has cycle problem

---
+++ linked lists/hasCycle.js
@@ -0,0 +1,23 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+
+var hasCycle = function (head) {
+  // If there is a cycle, the fast pointer will eventually lap the slow pointer and they will meet.
+  let slow = head;
+  let fast = head;
+
+  // If there is a cycle, the fast pointer will eventually lap the slow pointer and they will meet. In this case set to true
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+
+  // Else, set to false
+  return false;
+};
